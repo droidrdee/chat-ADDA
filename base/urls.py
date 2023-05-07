4bbe2b6d7828bc094f96e50ad71d4e2b5b1f2a26
Deepak Rathore <deepak.yourdomain@gmail.com>
2023-05-07 23:04:28 +0530

    Added a task updates file

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
 
-    path('', views.home, name="home"),
-    path('room/<str:pk>/', views.room, name="room"),
+    #path('', views.home, name="home"),
+   # path('room/<str:pk>/', views.room, name="room"),
 ]
